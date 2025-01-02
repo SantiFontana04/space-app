3074a52f9cc82fe23e130028b9d28fac00e9e1e9
SantiFontana <santiagofontana61@gmail.com>
2025-01-02 02:29:58 -0300

    Agregando fotos para la Galeria y haciendo una ventana modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-    import styled from "styled-components"
-    import GlobalStyles from "./Components/GlobalStyles"
-    import Cabecera from "./Components/Cabecera"
-    import BarraLateral from "./Components/BarraLateral"
-    import Banner from "./Components/Banner"
-    import banner from "./assets/banner.png"
+import styled from "styled-components"
+import GlobalStyles from "./Components/GlobalStyles"
+import Cabecera from "./Components/Cabecera"
+import BarraLateral from "./Components/BarraLateral"
+import Banner from "./Components/Banner"
+import banner from "./assets/banner.png"
 import Galeria from "./Components/Galeria"
+import fotos from "./fotos.json"
+import { useState } from "react"
+import ModalZoom from "./Components/ModalZoom"
 
     const FondoGradiente = styled.div`
     background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
     width: 100%;
     min-height: 100dvh;
     `
-
+    
     const AppContainer = styled.div`
     width: 1440px;
     max-width: 100%;
@@ -29,8 +32,9 @@ const ContenidoGaleria = styled.section`
     flex-grow: 1;
 `
 
-    function App() {
-    
+const App = () => {
+const [fotosDeGaleria, setFotosDeGaleria] = useState(fotos)
+const [fotoSeleccionada, setFotoSeleccionada] = useState(fotos[0])
 
     return (
         <>
@@ -43,10 +47,11 @@ const ContenidoGaleria = styled.section`
                     <ContenidoGaleria>
                         <Banner texto="La galería más completa de fotos del espacio" 
                         backgroundImage={banner} />
-                        <Galeria />
+                        <Galeria alSeleccionarFoto={foto => setFotoSeleccionada(foto)} fotos={fotosDeGaleria} />
                     </ContenidoGaleria>
             </MainContainer>
             </AppContainer>
+            <ModalZoom foto={fotoSeleccionada} />
         </FondoGradiente>
         </>
     )
